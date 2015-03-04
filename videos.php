15dfab8d14b3b998ec3e09ca96afaeac95d5ca19
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-03-04 16:12:34 -0500

    fixes spacing on videos.php...again...

--- videos.php
+++ videos.php
@@ -5,7 +5,7 @@ Template Name: Videos
 ?>
 <?php get_header(); ?>
 <div id="contentWrapper">
-	<div id="content">
+  <div id="content">
     <?php
       $page = get_page_by_path( 'videos' );
       $page_data = get_page($page->ID);
@@ -13,7 +13,7 @@ Template Name: Videos
       $content = apply_filters('the_content', $page_data->post_content);
       echo $content;
 		?>
-	</div><!-- end content-->
+  </div><!-- end content-->
   <?php get_sidebar(); ?>
 </div><!-- end contentWrap -->
 <?php get_footer(); ?>
